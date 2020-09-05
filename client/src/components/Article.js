35015d1b6c616fac93bed9e7453fe686026b93a9
DEBASISH <debasishkhan07@gmail.com>
2020-09-05 13:46:44 +0530

    first commit

---
+++ client/src/components/Article.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect }from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import spinner from '../components/810.gif'
+import { Link } from 'react-router-dom';
+
+const Article = props => {
+    const [title,setTitle] = useState('')
+    const [article, setArticle] = useState('')
+    const [authorname, setAuthorName] = useState('')
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/articles/${props.match.params.id}`)
+        .then(res => [
+            setTitle(res.data.title),
+            setArticle(res.data.article),
+            setAuthorName(res.data.authorname)
+        ])
+        .catch(error => console.log(error));
+    }, [props]);
+
+ return (
+    <MainContainer>
+        {!title || !article || !authorname ? (
+            <img src={spinner} alt="loading..." />
+        ):(
+        <>
+        <h2>{title}</h2>
+        <p>{article}</p>
+        <p className="badge badge-secondary">{authorname}</p>
+        <br />
+        <Link to="/" type="submit" className="btn btn-primary">Back to Home</Link>
+        </>
+        )}
+    </MainContainer>
+    );
+};
+
+export default Article
+
+//MAINCONTAINER
+
+const MainContainer =styled.div`
+    margin: 6rem auto;
+    padding: 3rem 14rem;
+
+    h2{
+        text-align: center;
+        font-weight: 900;
+        color: var(--dark-green);
+    }
+
+.btn-primary{
+        margin-top: 2rem;
+        background: var(--dark-green);
+        border: none;
+        &:hover {
+            background: var(--light-green);
+        }
+    }
+`;
---
+++ client/src/components/layouts/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../layouts/logo192.png';
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <NavbarContainer>
+            <nav className="navbar navbar-expand-lg navbar-light px-5 py-0">
+            <a className="navbar-brand" to="#">
+                <img src={logo} alt="logo" width="30" height="30" />
+            </a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+      
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/add-article">Add Article</Link>
+              
+            </li>
+            
+          </ul>
+            
+        </div>
+      </nav>
+                
+     </NavbarContainer>
+    )
+}
+
+export default Navbar;
+
+//MAIN NAVBAR CONTAINER
+const NavbarContainer = styled.div`
+    background: var(--dark-green);
+    .nav-link {
+        color: #fff !important;
+        &:hover {
+            background: var(--light-green);
+        }
+    }
+`;
